b9529e265b1faa970d9263445eee9a164a6482f5
Nageswar Padal <npadal848@gmail.com>
2021-03-16 14:09:11 +0530

    Added Array problmes solution dt-16/03/21

---
+++ src/array/ReverseAnArray.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Scanner;
+
+public class ReverseAnArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+	}
+
+	static int[] reverse(int[] arr, int n) {
+		
+		for (int i = 0; i < n; i++) {
+			
+		}
+
+		return arr;
+	}
+}
