daeb2dfc9984d6fb97ed756bb7fc4ac741b59ad1
Nageswar Padal <npadal848@gmail.com>
2021-03-19 10:09:55 +0530

    Added Array problmes solution dt-18/03/21

---
+++ src/array/LeaderInArray.java
@@ -0,0 +1,53 @@
+package array;
+
+import java.util.Scanner;
+
+public class LeaderInArray {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+//		findLeader1(arr, n);
+		findLeader2(arr, n - 1);
+	}
+
+	/*
+	 * Method-1 TC - O(n^2), SC - Theta(1)
+	 */
+	static void findLeader1(int[] arr, int n) {
+		for (int i = 0; i < n - 1; i++) {
+			boolean flag = true;
+			for (int j = i + 1; j < n; j++) {
+				if (arr[i] <= arr[j]) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) {
+				System.out.print(arr[i] + ", ");
+			}
+		}
+		System.out.print(arr[n - 1]);
+	}
+
+	/*
+	 * Method-1 TC - O(n^2), SC - Theta(1)
+	 */
+	static void findLeader2(int[] arr, int n) {
+		int currLeader = arr[n];
+		System.out.print(currLeader + " ");
+		for (int i = n - 1; i >= 0; i--) {
+			if (currLeader < arr[i]) {
+				System.out.print(arr[i] + " ");
+				currLeader = arr[i];
+			}
+		}
+	}
+
+}
--- src/array/LeftRotateAnArrayByDPlace.java
+++ src/array/LeftRotateAnArrayByDPlace.java
@@ -13,9 +13,15 @@ public class LeftRotateAnArrayByDPlace {
 			arr[i] = sc.nextInt();
 		}
 
-		leftRotateWithExtraSpace(arr, n, d);
+//		leftRotateWithExtraSpace(arr, n, d);
+//		rotate(arr, n, d);
+		
+		leftRotateBetterSol(arr, n, d);
 	}
 
+	/*
+	 * Method-1 TC- Theta(n) SP-Theta(d)
+	 */
 	static void leftRotateWithExtraSpace(int[] arr, int n, int d) {
 
 		int[] temp = new int[d];
@@ -30,12 +36,33 @@ public class LeftRotateAnArrayByDPlace {
 		for (int j = i; j < n; j++) {
 			arr[j - d] = arr[j];
 		}
-		int start=n-d;
-		//rotate array to left
+		int start = n - d;
+		// rotate array to left
 		for (i = 0; i < d; i++) {
-			arr[i+start] = temp[i];
+			arr[i + start] = temp[i];
 		}
 		print(arr);
+	}
+
+	/*
+	 * Method-2 TC- Theta(nd) SP-Theta(1)
+	 */
+	static void rotate(int[] arr, int n, int d) {
+		for (int i = 0; i < d; i++) {
+			leftRotate(arr, n);
+		}
+
+		print(arr);
+	}
+
+	static void leftRotate(int[] arr, int n) {
+
+		int temp = arr[0];
+
+		for (int i = 1; i < n; i++) {
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = temp;
 	}
 
 	static void print(int[] arr) {
@@ -43,5 +70,27 @@ public class LeftRotateAnArrayByDPlace {
 			System.out.print(ele + ", ");
 		}
 		System.out.println();
+	}
+
+	/*
+	 * Method-3 TC- Theta(n) SP-Theta(1)
+	 */
+	static void leftRotateBetterSol(int[] arr, int n, int d) {
+		reverse(arr, 0, d - 1);
+		reverse(arr, d, n - 1);
+		reverse(arr, 0, n - 1);
+		
+		print(arr);
+	}
+
+	static void reverse(int[] arr, int start, int end) {
+		
+		while (start < end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
 	}
 }
