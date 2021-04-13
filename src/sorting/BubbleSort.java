64775f677d3741133e336cf257d63c8b8145b446
Nageswar Padal <npadal848@gmail.com>
2021-04-13 11:09:06 +0530

    Added sorting problems

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,45 @@
+package sorting;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		print(bubuleSort(arr, n));
+	}
+
+	static int[] bubuleSort(int[] arr, int n) {
+		for (int i = 0; i < n; i++) {
+			boolean flag = false;
+			for (int j = 0; j < (n - i) - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					flag = true;
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+			if (!flag)
+				break;
+		}
+		return arr;
+	}
+
+	static void print(int[] arr) {
+		for (int num : arr)
+			System.out.print(num + " ");
+		System.out.println();
+	}
+}
