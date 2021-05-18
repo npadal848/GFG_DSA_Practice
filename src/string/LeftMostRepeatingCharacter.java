f4141de9c0539c15c639a32d77ae3164d615417e
Nageswar Padal <npadal848@gmail.com>
2021-05-19 02:14:56 +0530

    Added String problems

--- src/string/LeftMostRepeatingCharacter.java
+++ src/string/LeftMostRepeatingCharacter.java
@@ -1,5 +1,7 @@
 package string;
 
+import java.util.Arrays;
+
 public class LeftMostRepeatingCharacter {
 
 	public static void main(String[] args) {
@@ -14,6 +16,14 @@ public class LeftMostRepeatingCharacter {
 		System.out.println(find2(s1));
 		System.out.println(find2(s2));
 		System.out.println(find2(s3));
+		System.out.println();
+		System.out.println(find3(s1));
+		System.out.println(find3(s2));
+		System.out.println(find3(s3));
+		System.out.println();
+		System.out.println(find4(s1));
+		System.out.println(find4(s2));
+		System.out.println(find4(s3));
 	}
 
 //	TC - O(n^2) and SC - O(1)
@@ -27,7 +37,7 @@ public class LeftMostRepeatingCharacter {
 		return -1;
 	}
 
-//	TC - O(n) and SC - O(1)
+//	TC - Theta(n+char) and SC - Theta(char)
 	static int find2(String s) {
 		int[] count = new int[256];
 		for (int i = 0; i < s.length(); i++) {
@@ -43,13 +53,37 @@ public class LeftMostRepeatingCharacter {
 
 //	TC - O(n) and SC - O(1)
 	static int find3(String s) {
-		int[] count = new int[256];
+		int res = Integer.MAX_VALUE;
+		int[] firstIndex = new int[256];
+		Arrays.fill(firstIndex, -1);
+
 		for (int i = 0; i < s.length(); i++) {
-			count[s.charAt(i)]++;
-			if (count[s.charAt(i)] > 1)
-				return i;
+			if (firstIndex[s.charAt(i)] == -1)
+				firstIndex[s.charAt(i)] = i;
+			else
+				res = Math.min(res, firstIndex[s.charAt(i)]);
 
 		}
-		return -1;
+		if (res == Integer.MAX_VALUE)
+			return -1;
+		return res;
+	}
+
+//	TC - O(n) and SC - O(1)
+	static int find4(String s) {
+		int res = Integer.MAX_VALUE;
+		boolean[] flags = new boolean[256];
+		Arrays.fill(flags, false);
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (flags[s.charAt(i)])
+				res = Math.min(res, i);
+			else
+				flags[s.charAt(i)] = true;
+
+		}
+		if (res == Integer.MAX_VALUE)
+			return -1;
+		return res;
 	}
 }
