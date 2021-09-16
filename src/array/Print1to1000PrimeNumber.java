dec5e7535638f251127bee23e63d73dd152c93eb
Nageswar Padal <npadal848@gmail.com>
2021-09-16 11:58:33 +0530

    updated condition

--- src/array/Print1to1000PrimeNumber.java
+++ src/array/Print1to1000PrimeNumber.java
@@ -3,7 +3,7 @@ package array;
 public class Print1to1000PrimeNumber {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 1000; i++) {
+		for (int i = 2; i <= 1000; i++) {
 			boolean isPrime = true;
 			for (int j = 2; j < i; j++) {
 				if (i % j == 0) {
